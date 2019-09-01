d7f1502022e01557726994455dbef0252463374b
Michael Bryan <michaelfbryan@gmail.com>
2019-09-02 01:14:26 +0800

    Added mermaid graphing

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+
+window.onload = () => {
+    var config = {
+        startOnLoad: true,
+        flowchart: {
+            useMaxWidth: false,
+            htmlLabels: true
+        }
+    };
+    mermaid.initialize(config);
+};
