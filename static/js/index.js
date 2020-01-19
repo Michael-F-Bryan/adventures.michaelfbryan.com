e4aae57a873856fc9ffa168e56dd15b4d1d249f2
Michael Bryan <michaelfbryan@gmail.com>
2020-01-19 15:53:19 +0800

    Fiddled with styling so headers get nice permalinks

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,15 @@
+/**
+ * Schedules a function to be called after the DOM is loaded.
+ */
+function onLoaded(cb) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(cb, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", cb);
+    }
+}
 
-window.onload = () => {
+function initializeMermaid() {
     var config = {
         startOnLoad: true,
         flowchart: {
@@ -8,4 +18,31 @@ window.onload = () => {
         }
     };
     mermaid.initialize(config);
-};
+}
+
+/**
+ * The current theme doesn't let you get a link to a header by just clicking on
+ * it.
+ */
+function addPermalinks() {
+    document.querySelectorAll("h2, h3, h4, h5, h6")
+        .forEach(applyPermalink);
+}
+
+function applyPermalink(element) {
+    if (element.id) {
+        const link = document.createElement("a");
+        link.href = "#" + element.id;
+
+        while (element.firstChild) {
+            const child = element.firstChild;
+            element.removeChild(child);
+            link.appendChild(child);
+        }
+
+        element.appendChild(link);
+    }
+}
+
+onLoaded(initializeMermaid);
+onLoaded(addPermalinks);
